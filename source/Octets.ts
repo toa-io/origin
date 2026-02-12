fbd73a2450c905349301a4ed2e7d77493651a3ab
temich <tema.gurtovoy@gmail.com>
2026-02-13 00:14:40 +0400

    feat: add `Workflow` type

--- source/Octets.ts
+++ source/Octets.ts
@@ -1,4 +1,5 @@
 import type { Err } from 'error-value'
+import type { Emitter } from 'mitt'
 
 export interface OctetsEntry {
   id: string
@@ -12,3 +13,5 @@ export interface WorkflowStep<K extends string = string, T = unknown, E extends 
 }
 
 export type Faulty<T extends Record<string, unknown>> = Record<keyof T, T[keyof T] | Err>
+
+export type Workflow<T extends Record<string, unknown> = Record<string, unknown>> = [OctetsEntry, Emitter<Faulty<T>>]
